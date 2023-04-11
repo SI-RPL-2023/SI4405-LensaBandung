2d9154c28f403c7e12f4509b8997f8566b19b56b
nafiyah99 <nafiyah@student.telkomuniversity.ac.id>
2023-04-11 15:53:43 +0700

    admin dan home controller

---
+++ laravel/app/Http/Controllers/AdminController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pengaduan;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.index');
+    }
+
+    public function detail()
+    {
+        $data = Pengaduan::all();
+        return view('admin.detailpengaduan', compact('data'));
+    }
+
+    public function accept($id)
+    {
+        $pengaduan = Pengaduan::findorfail($id);
+        $pengaduan->update([
+            'status' => "Accepted",
+        ]);
+
+        return redirect()->back()->with('info', 'Pengaduan berhasil diaccept');
+    }
+
+    public function denied($id)
+    {
+        
+        $pengaduan = Pengaduan::findorfail($id);
+        $pengaduan->update([
+            'status' => "Denied",
+        ]);
+
+        return redirect()->back()->with('info', 'Pengaduan berhasil direject');
+    }
+}
